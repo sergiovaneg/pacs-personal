30332d04c8565dbb5dcbb787b9e468269c3bd9ee
Sergio Mauricio Vanegas Arias <sergiovaneg@gmail.com>
2021-04-11 16:43:39 +0000

    'type_traits' replacing 'dummy typename' in the implementation

--- Template_Norms/Norms.hpp
+++ Template_Norms/Norms.hpp
@@ -4,41 +4,37 @@
 #include <algorithm>
 #include <numeric>
 #include <cmath>
+#include <type_traits>
 
 template <typename Container>
 class Norms
 {
     const Container & myContainer;
 
-    template <unsigned int p, typename FAKE = void> struct Aux {
-        static auto compute(const Container & myC) {
-            return std::pow(std::accumulate(myC.cbegin(), myC.cend(), 0.,
+    template <unsigned p>
+    auto compute(std::integral_constant<unsigned, p>) const
+    {
+        return std::pow(std::accumulate(myContainer.cbegin(), myContainer.cend(), 0.,
                         [](auto const & x, auto const & y) {return x + std::pow(std::abs(y), p);}),
                         1./p);
-        }
-    };
+    }
 
-    template <typename FAKE> struct Aux<0, FAKE> {
-        static auto compute(const Container & myC) {
-            return std::count_if(myC.cbegin(), myC.cend(),
-                                        [](auto i){return i;});
-        }
-    };
+    auto compute(std::integral_constant<unsigned, 0>) const{
+        return std::count_if(myContainer.cbegin(), myContainer.cend(), [](auto i){return i;});
+    }
 
-    template <typename FAKE> struct Aux<1, FAKE> {
-        static auto compute(const Container & myC) {
-            return std::accumulate(myC.cbegin(), myC.cend(), 0.,
-                        [](auto const & x, auto const & y) {return x + std::abs(y);});
-        }
-    };
+    auto compute(std::integral_constant<unsigned, 1>) const{
+        return std::accumulate(myContainer.cbegin(), myContainer.cend(), 0.,
+                                [](auto const & x, auto const & y) {return x + std::abs(y);});
+    }
 
 public:
     Norms(Container const & c) : myContainer{c} {};
 
-    template <unsigned int p>
+    template <unsigned p>
     auto compute() const
     {       
-        return Aux<p>::compute(myContainer);
+        return compute(std::integral_constant<unsigned, p>());
     }
 };
 
